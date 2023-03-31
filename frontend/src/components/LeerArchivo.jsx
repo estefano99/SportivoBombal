92005a8043e1964080748dcc7ed6fc7f49e9ea50
JuliBosch <julibosch99@gmail.com>
2023-03-31 16:26:35 -0300

    Revert "Merge branch 'master' of https://github.com/estefano99/SportivoBombal"
    This reverts commit 13a587a090123c9c82986e18968ed34bd4c8b1b8, reversing
    changes made to b6f6a197f3e6235e9ae25047546dd64f13289ea0.

--- frontend/src/components/LeerArchivo.jsx
+++ frontend/src/components/LeerArchivo.jsx
@@ -1,18 +1,15 @@
 import React, { useState, useEffect } from "react";
 import * as XLSX from "xlsx";
 import clienteAxios from "../config/axios";
-import ListaSociosErroneos from "./ListaSociosErroneos";
 
 function LeerArchivo() {
   const [socios1, setSocios1] = useState([]);
   const [socios2, setSocios2] = useState([]);
   const [estadoBoton, setEstadoBoton] = useState(true);
-  const [sociosErroneo, setSociosErroneo] = useState([]); //Guarda los socios que tienen mal ingresado el dni
 
   const handleFileUpload = (e) => {
     const archivo = e.target.files[0];
     const reader = new FileReader();
-
 
     reader.onload = (e) => {
       const workbook = XLSX.read(e.target.result, { type: "binary" });
@@ -27,38 +24,12 @@ function LeerArchivo() {
       // Recorre los datos de la hoja y crea un objeto para cada socio
       for (let i = 1; i < primeraMitad; i++) {
         const [codigo, nombreCompleto, cuotasAdeudadas, dni] = datos[i];
-
-        if (!dni || dni.length > 8 || dni.length < 8) {
-          
-          const nuevoErroneo = {
-            nombreCompleto,
-            dni
-          }
-
-          setSociosErroneo((prev) =>[...prev,nuevoErroneo]); // agrega un nuevo socio erróneo al arreglo
-
-        } else {
-
-          nuevosSocios1.push({ codigo, nombreCompleto, cuotasAdeudadas, dni });
-        }
+        nuevosSocios1.push({ codigo, nombreCompleto, cuotasAdeudadas, dni });
       };
 
       for (let i = primeraMitad; i < datos.length; i++) {
         const [codigo, nombreCompleto, cuotasAdeudadas, dni] = datos[i];
-
-        if (!dni || dni.length > 8 || dni.length < 8) {
-
-          const nuevoErroneo = {
-            nombreCompleto,
-            dni
-          }
-          
-         setSociosErroneo((prev) =>[...prev,nuevoErroneo]); // agrega un nuevo socio erróneo al arreglo
-
-        } else{
-
-          nuevosSocios2.push({ codigo, nombreCompleto, cuotasAdeudadas, dni });
-        }
+        nuevosSocios2.push({ codigo, nombreCompleto, cuotasAdeudadas, dni });
       };
 
       setSocios1(nuevosSocios1);
@@ -66,7 +37,6 @@ function LeerArchivo() {
     };
 
     reader.readAsBinaryString(archivo);
-
   };
 
   const validarDNI = (arregloSocios) => {
@@ -81,7 +51,6 @@ function LeerArchivo() {
   };
 
   const sendData = async (socios) => {
-
     try {
       const response = await clienteAxios.post('/admin/cargar-archivo', socios);
       console.log(response); // respuesta del servidor
@@ -91,7 +60,6 @@ function LeerArchivo() {
   };
 
   const handleSubmitClick = async () => {
-
     try {
       const promise1 = sendData(socios1);
       const promise2 = sendData(socios2);
@@ -104,7 +72,6 @@ function LeerArchivo() {
 
   useEffect(() => {
 
-    //Primero pregunta si socios fue cargado, despues si hay socios mal cargados, y depende de si hay (socios erroneos o no) el cambio del Boton.
     if (socios1.length) {
       const arregloSocios = [...socios1, ...socios2];
 
@@ -126,12 +93,6 @@ function LeerArchivo() {
       >
         Subir Archivo
       </button>
-      {
-        sociosErroneo && 
-        sociosErroneo.map((socio,index) => (
-            <ListaSociosErroneos key={index} socio={socio}  />
-          ))
-      }
     </div>
   );
 }
