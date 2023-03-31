1a0a0f442e250f6905dc880407ea6b0bfaa82290
JuliBosch <julibosch99@gmail.com>
2023-03-31 16:32:41 -0300

    Cagadita

--- frontend/src/components/LeerArchivo.jsx
+++ frontend/src/components/LeerArchivo.jsx
@@ -1,35 +1,63 @@
-import React, { useState, useEffect } from "react";
-import * as XLSX from "xlsx";
 import clienteAxios from "../config/axios";
+import ListaSociosErroneos from "./ListaSociosErroneos";
 
 function LeerArchivo() {
   const [socios1, setSocios1] = useState([]);
   const [socios2, setSocios2] = useState([]);
   const [estadoBoton, setEstadoBoton] = useState(true);
+  const [sociosErroneo, setSociosErroneo] = useState([]); //Guarda los socios que tienen mal ingresado el dni
 
   const handleFileUpload = (e) => {
     const archivo = e.target.files[0];
     const reader = new FileReader();
 
+
     reader.onload = (e) => {
       const workbook = XLSX.read(e.target.result, { type: "binary" });
       const hoja = workbook.Sheets[workbook.SheetNames[0]];
       const datos = XLSX.utils.sheet_to_json(hoja, { header: 1 });
-
+      
       const nuevosSocios1 = [];
       const nuevosSocios2 = [];
 
-      const primeraMitad = parseInt(datos.length / 2);
+      //! DUDA SOBRE ESTO, y si son impares que pasa? toma uno demas o uno menos en la division?
+      const primeraMitad = parseInt(datos.length/2);
 
       // Recorre los datos de la hoja y crea un objeto para cada socio
       for (let i = 1; i < primeraMitad; i++) {
         const [codigo, nombreCompleto, cuotasAdeudadas, dni] = datos[i];
-        nuevosSocios1.push({ codigo, nombreCompleto, cuotasAdeudadas, dni });
+
+        if (!dni || dni.length > 8 || dni.length < 8) {
+          
+          const nuevoErroneo = {
+            nombreCompleto,
+            dni
+          }
+
+          setSociosErroneo((prev) =>[...prev,nuevoErroneo]); // agrega un nuevo socio erróneo al arreglo
+
+        } else {
+
+          nuevosSocios1.push({ codigo, nombreCompleto, cuotasAdeudadas, dni });
+        }
       };
 
       for (let i = primeraMitad; i < datos.length; i++) {
         const [codigo, nombreCompleto, cuotasAdeudadas, dni] = datos[i];
-        nuevosSocios2.push({ codigo, nombreCompleto, cuotasAdeudadas, dni });
+
+        if (!dni || dni.length > 8 || dni.length < 8) {
+
+          const nuevoErroneo = {
+            nombreCompleto,
+            dni
+          }
+          
+         setSociosErroneo((prev) =>[...prev,nuevoErroneo]); // agrega un nuevo socio erróneo al arreglo
+
+        } else{
+
+          nuevosSocios2.push({ codigo, nombreCompleto, cuotasAdeudadas, dni });
+        }
       };
 
       setSocios1(nuevosSocios1);
@@ -37,29 +65,21 @@ function LeerArchivo() {
     };
 
     reader.readAsBinaryString(archivo);
-  };
 
-  const validarDNI = (arregloSocios) => {
-    arregloSocios.forEach(socio => {
-      const {dni, nombreCompleto} = socio;
-
-      if(dni === '' || dni.length > 8) {
-        console.log(`LA RE CONCHA DE TU MADRE: ${nombreCompleto}`);
-      };
-
-    });
   };
 
   const sendData = async (socios) => {
+
     try {
       const response = await clienteAxios.post('/admin/cargar-archivo', socios);
-      console.log(response); // respuesta del servidor
+      console.log('Alalala'); // respuesta del servidor
     } catch (error) {
       console.log(error);
     }
   };
 
   const handleSubmitClick = async () => {
+
     try {
       const promise1 = sendData(socios1);
       const promise2 = sendData(socios2);
@@ -72,14 +92,16 @@ function LeerArchivo() {
 
   useEffect(() => {
 
+    //Primero pregunta si socios fue cargado, despues si hay socios mal cargados, y depende de si hay (socios erroneos o no) el cambio del Boton.
     if (socios1.length) {
-      const arregloSocios = [...socios1, ...socios2];
+      if (sociosErroneo.length > 0) {
+        setEstadoBoton(true)
+      }else{
+        setEstadoBoton(false)
+      }
+    }
 
-      console.log(arregloSocios);
-      setEstadoBoton(false);
-    };
-
-  }, [socios1, socios2]);
+  }, [socios1,sociosErroneo])
 
 
   return (
@@ -93,6 +115,12 @@ function LeerArchivo() {
       >
         Subir Archivo
       </button>
+      {
+        sociosErroneo && 
+        sociosErroneo.map((socio,index) => (
+            <ListaSociosErroneos key={index} socio={socio}  />
+          ))
+      }
     </div>
   );
 }
