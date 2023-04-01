4a285a14a8a2c8b0ebb035f749b6327d02d49c79
JuliBosch <julibosch99@gmail.com>
2023-03-31 23:44:50 -0300

    Pagina de inicio del admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import LeerArchivo from './pages/LeerArchivo';
 import UsuariosLayout from './layout/UsuariosLayout';
 import AdminsLayout from './layout/AdminsLayout';
+import AdminInicio from './pages/AdminInicio';
 import Login from './pages/Login';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         </Route>
 
         <Route path='/admin' element={<AdminsLayout />}>
+          <Route index element={<AdminInicio />}/>
           <Route path='/admin/cargar-archivo' element={<LeerArchivo />}/>
         </Route>
       </Routes>
---
+++ frontend/src/pages/AdminInicio.jsx
@@ -0,0 +1,12 @@
+const AdminInicio = () => {
+  return (
+    <>
+      <h2>PAGINA DE INICIO DE LOS ADMINS</h2>
+      <p>ADMIN</p> 
+      <p>ADMIN</p> 
+      <p>ADMIN</p> 
+    </>
+  )
+}
+
+export default AdminInicio;
